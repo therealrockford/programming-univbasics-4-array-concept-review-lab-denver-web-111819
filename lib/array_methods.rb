6b3cfe60bc295e50f546f83f10ee256caed56a58
Rocky Pitto <rockypitto@gmail.com>
2019-10-16 20:09:17 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -2,8 +2,9 @@ def find_element_index(array, value_to_find)
   array.length.times do |count|
     if array[count] == value_to_find
       return count
-    end 
-  end 
+    end
+    nil
+  end
 end
 
 def find_max_value(array)
